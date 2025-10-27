00aebc8f220426e034b0ea14212cae63e8c24a3c
Kamesh A <akamesh.work@gmail.com>
2025-10-28 01:01:31 +0530

    Prepare release: build and asset fixes

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b51349b7fabebbc847b4451ee2c0cf2c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "8422a40569911eaa5b3a9692b3c1d4ce",
+"flutter_bootstrap.js": "79ffde9719129b0909797c62b3e24608",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
